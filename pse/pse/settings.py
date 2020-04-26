7f8cc73113368e7ada3d16ed0b3cf72ec15bb94c
Valeria Stromtsova <v.stromtsova@yandex.ru>
2020-04-26 23:46:45 +0300

    Removed unnecessary changes in settings.py

--- pse/pse/settings.py
+++ pse/pse/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -27,6 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '0.0.0.0'
 ]
+
 
 # Application definition
 
@@ -70,6 +72,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pse.wsgi.application'
 
+
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -93,6 +96,7 @@ DATABASES = {
     }
 }
 
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -111,6 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -123,6 +128,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
