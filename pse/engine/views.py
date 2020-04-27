a9b69abb85804cc1405edf4787dff333333d348b
George <gemiti@uahoo.com>
2020-04-27 21:29:35 +0300

    added fast-search

--- pse/engine/views.py
+++ pse/engine/views.py
@@ -2,9 +2,11 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from engine.models import Document, Page
-import re
 from rest_framework import status
 from rest_framework.parsers import JSONParser
+
+import re
+import json
 
 
 def index(request):
@@ -17,7 +19,7 @@ def fast_search(request):
         data = JSONParser().parse(request)
         name = data['name']
         keywords = data['keywords']
-        docs = [i for i in Document.objects.mongo_aggregate([
+        ods = [i for i in Document.objects.mongo_aggregate([
             {'$match': {'name': name}},
             {'$project': {
                 'pages': {
@@ -36,8 +38,16 @@ def fast_search(request):
             {'$unset': ['pages.text', 'pages.vision']}
         ])]
         # Add conversion to json
-        print(docs[0])
-        return HttpResponse("HelloWorld")
+        results = dict()
+        for od in ods:
+            pages = dict(od)['pages']
+            found_in_document = dict()
+            for p in pages:
+                p = dict(p)
+                found_in_document[p['num']] = p['url']
+            results[name] = found_in_document
+
+        return JsonResponse(results, status=status.HTTP_200_OK)
 
 
 @csrf_exempt
