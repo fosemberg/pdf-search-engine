bd4161dfcbdb8246fbee9a58dd8325a857f03bde
fosemberg <fosemberg@gmail.com>
2020-04-29 23:41:42 +0300

    integrate-front-with-backend-search delete no-cors mode

--- front/src/store/store.ts
+++ front/src/store/store.ts
@@ -1,28 +1,33 @@
 import {
-    SearchResponse,
-    SearchRequest
+  SearchResponse,
+  SearchRequest
 } from "../utils/apiTypes";
 import {SERVER_HOST, SERVER_HTTP_PORT} from "../config/env";
 
 const hostUrl = `${SERVER_HOST}:${SERVER_HTTP_PORT}`;
 
 export const sendSearchRequest = async (searchRequest: SearchRequest): Promise<SearchResponse> => {
-    const url = 'slow-search';
-    const _url = `${hostUrl}/${url}`;
-    return fetch(
-        `${_url}`,
-        {
-            method: 'POST',
-            mode: 'no-cors',
-            body: JSON.stringify(searchRequest)
-        }
+  const url = 'slow-search';
+  const _url = `${hostUrl}/${url}`;
+  try {
+    const response = await fetch(
+      `${_url}`,
+      {
+        method: 'POST',
+        body: JSON.stringify(searchRequest)
+      }
     )
-        .then((res) => res.json())
-        // TODO: delete when will be backend
-        .catch(() => ({
-            "NUP4114": {
-                "2": "https://github.com/antosha417/test_temp/raw/master/NUP4114/NUP4114-2.pdf",
-                "3": "https://github.com/antosha417/test_temp/raw/master/NUP4114/NUP4114-3.pdf"
-            }
-        }))
-};
+    console.log(response)
+    const json = await response.json()
+    console.log(json)
+    return json;
+  } catch (error) {
+    console.error(error);
+    return {
+      "NUP4114": {
+        "2": "https://github.com/antosha417/test_temp/raw/master/NUP4114/NUP4114-2.pdf",
+        "3": "https://github.com/antosha417/test_temp/raw/master/NUP4114/NUP4114-3.pdf"
+      }
+    }
+  }
+}
