71bd2291b6748c8275007e27e9d6105b7461db29
Valeria Stromtsova <v.stromtsova@yandex.ru>
2020-05-01 18:02:44 +0300

    Fixed POST file upload

--- pse/engine/views.py
+++ pse/engine/views.py
@@ -90,43 +90,33 @@ def slow_search(request):
 
 @csrf_exempt
 def upload(request):
-    if request.method == 'GET':
-        # data = JSONParser().parse(request)
-        # document_name = data['filename']
-        document_name = 'test'
-        # if request.FILES[document_name]:
-        # pdf_file = request.FILES[document_name]
-        workpath = os.path.dirname(os.path.abspath(__file__))
-        with open(os.path.join(workpath, 'test.pdf'), 'rb') as pdf_file:
-
-
-
-            # saving pages
-            pdf_pages = pdf_parser.split_file_to_pages(os.path.join(workpath, 'test.pdf'))
-            print(pdf_pages)
-            pages = []
-            for i in range(len(pdf_pages)):
-                vision, text = pdf_parser.parse_pdf(pdf_pages[i])
-                url = storage_upload.fileobj2url(pdf_pages[i], '{}_page_{}'.format(document_name, i))
-                if url['error'] is not None:
-                    return HttpResponse('Unable to load the file', status=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE)
-                # TODO: this line below causes exception: pdf_pages[i] should be open!
-                pages.append(
-                    Page(
-                        url=url['url'],
-                        num=i + 1,
-                        text=text,
-                        vision=vision
-                    )
+    if request.method == 'POST':
+        pdf_file = request.FILES['file']
+        document_name = request.POST['filename']
+        pdf_pages = pdf_parser.split_file_to_pages(pdf_file)
+        pages = []
+        for i in range(len(pdf_pages)):
+            vision, text = pdf_parser.parse_pdf(pdf_pages[i])
+            url = storage_upload.fileobj2url(pdf_pages[i], '{}_page_{}'.format(document_name, i))
+            if url['error'] is not None:
+                return HttpResponse('Unable to load the file', status=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE)
+            pages.append(
+                Page(
+                    url=url['url'],
+                    num=i + 1,
+                    text=text,
+                    vision=vision
                 )
+            )
 
-            # saving document to storage
-            document_url = storage_upload.fileobj2url(pdf_file, document_name)
-            if document_url['error'] is not None:
-                return HttpResponse('Unable to load the file', status=status.HTTP_424_FAILED_DEPENDENCY)
+        # saving document to storage
+        document_url = storage_upload.fileobj2url(pdf_file, document_name)
+        if document_url['error'] is not None:
+            return HttpResponse('Unable to load the file', status=status.HTTP_424_FAILED_DEPENDENCY)
 
-            # saving document
-            d = Document(name='NUP4114', url=document_url, pages=pages)
-            d.save()
-            return HttpResponse(status=status.HTTP_201_CREATED)
+        # saving document
+        d = Document(name=document_name, url=document_url, pages=pages)
+        d.save()
+        return HttpResponse(status=status.HTTP_201_CREATED)
+
     return HttpResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
