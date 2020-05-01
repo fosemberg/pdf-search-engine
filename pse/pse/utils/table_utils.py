f97a1e3574c895c2830199ce57406b846f2ecf79
George <gemiti@uahoo.com>
2020-05-02 01:38:38 +0300

    added table utils to extract tables from pdfs, added new table field to model

---
+++ pse/pse/utils/table_utils.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import pdfplumber
+from utils import storage_upload
+
+
+def extract_table_to_dfs(page, threshold):
+    """
+    :param page: pdf page in bytes
+    :param threshold: max percentage of nans
+    :return: list of pandas DataFrames
+    """
+    with pdfplumber.load(page) as pdf_page:
+        tables = pdf_page.pages[0].extract_tables()
+        dfs = []
+        for table in tables:
+            df = pd.DataFrame(table[1:], columns=table[0])
+            n, m = df.shape
+            number_of_nans = df.isnull().sum().sum()
+            if number_of_nans / (n * m) <= threshold:
+                dfs.append(df)
+        return dfs
+
+
+# likely to change if not using urls!
+def save_df_as_csv_to_storage(df, name):
+    csv = df.to_csv()
+    key = '{}_key'.format(name)
+    return storage_upload. file2url(name, key)
