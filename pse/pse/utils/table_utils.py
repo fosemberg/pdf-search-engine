8ee1e004ffa0ee921b75c76434ff7c692775122a
George <gemiti@uahoo.com>
2020-05-02 22:09:47 +0300

    now csvs are correctly uploaded to storage, tested locally

--- pse/pse/utils/table_utils.py
+++ pse/pse/utils/table_utils.py
@@ -1,9 +1,22 @@
 import pandas as pd
 import pdfplumber
+import io
+import numpy as np
 from utils import storage_upload
 
 
-def extract_table_to_dfs(page, threshold):
+NANS_THRESHOLD = 0.75
+
+
+def save_tables_from_page(page, page_number):
+    dfs = extract_table_to_dfs(page)
+    table_number = 0
+    for df in dfs:
+        save_df_as_csv_to_storage(df, 'page_{}_table_{}.csv'.format(page_number, table_number))
+        table_number += 1
+
+
+def extract_table_to_dfs(page):
     """
     :param page: pdf page in bytes
     :param threshold: max percentage of nans
@@ -14,16 +27,19 @@ def extract_table_to_dfs(page, threshold):
         dfs = []
         for table in tables:
             df = pd.DataFrame(table[1:], columns=table[0])
-            n, m = df.shape
-            number_of_nans = df.isnull().sum().sum()
-            if number_of_nans / (n * m) <= threshold:
-                dfs.append(df)
+            number_of_not_nans = np.sum(df.count())
+            if number_of_not_nans > 0:
+                number_of_nans = df.isnull().sum().sum()
+                percentage_of_nans = number_of_nans / (number_of_not_nans + number_of_nans)
+                if percentage_of_nans <= NANS_THRESHOLD:
+                    dfs.append(df)
     return dfs
 
 
 # likely to change if not using urls but keys!
 # also data can be lost when converting to csv, fix?
 def save_df_as_csv_to_storage(df, name):
-    csv = df.to_csv()
-    key = '{}_key'.format(name)
-    return storage_upload.fileobj2url(csv, key)
+    towrite = io.BytesIO()
+    df.to_excel(towrite)
+    towrite.seek(0)
+    return storage_upload.fileobj2url(towrite, name)
