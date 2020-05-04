d6b7b191f21b9e4d40c43b46dfe2422d33899e58
George <gemiti@uahoo.com>
2020-05-04 21:28:06 +0300

    added package with image extraction

---
+++ pse/pse/utils/image_utils.py
@@ -0,0 +1,75 @@
+import io
+import os
+import cv2
+import numpy as np
+from wand.image import Image, Color
+from PyPDF2 import PdfFileReader, PdfFileWriter
+from utils import storage_upload
+
+
+def zero_runs(a):
+    is_zero = np.concatenate(([0], np.equal(a, 0).view(np.int8), [0]))
+    abs_diff = np.abs(np.diff(is_zero))
+    ranges = np.where(abs_diff == 1)[0].reshape(-1, 2)
+    return ranges
+
+
+def extract_images(pdf_file):
+    infile = PdfFileReader(pdf_file)
+    for i in range(infile.getNumPages()):
+        output_pdf = PdfFileWriter()
+        output_pdf.addPage(infile.getPage(i))
+        workpath = os.path.dirname(os.path.abspath(__file__))
+        tem_pdf_path = os.path.join(workpath, 'temp_pdf.pdf')
+        with open(tem_pdf_path, "wb") as out_file:
+            output_pdf.write(out_file)
+        with Image(filename=os.path.join(workpath, 'test.pdf'), resolution=300) as img:
+            with Image(width=img.width, height=img.height, background=Color("white")) as bg:
+                bg.composite(img, 0, 0)
+                bg.save(filename="tmp.png")
+
+        img = cv2.imread('tmp.png', cv2.IMREAD_COLOR)
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img_gray_inverted = 255 - img_gray
+
+        row_means = cv2.reduce(img_gray_inverted, 1, cv2.REDUCE_AVG, dtype=cv2.CV_32F).flatten()
+        row_gaps = zero_runs(row_means)
+        row_cutpoints = (row_gaps[:, 0] + row_gaps[:, 1] - 1) // 2
+
+        for n, (y1, y2) in enumerate(zip(row_cutpoints, row_cutpoints[1:])):
+            y1 = int(y1)
+            y2 = int(y2)
+
+            line_gray_inverted = img_gray_inverted[y1:y2]
+
+            column_means = cv2.reduce(line_gray_inverted, 0, cv2.REDUCE_AVG, dtype=cv2.CV_32F).flatten()
+            column_gaps = zero_runs(column_means)
+            column_gap_sizes = column_gaps[:, 1] - column_gaps[:, 0]
+            column_cutpoints = (column_gaps[:, 0] + column_gaps[:, 1] - 1) // 2
+
+            filtered_cutpoints = column_cutpoints[column_gap_sizes > 15]
+
+            img_num = 0
+            for x1, x2 in zip(filtered_cutpoints, filtered_cutpoints[1:]):
+
+                # METRICS
+                selected_column_means = column_means[x1:x2]
+                non_0 = list(filter(None, selected_column_means))
+                mean = np.sum(non_0) / len(non_0)
+                max = np.max(non_0)
+
+                if mean / max < 0.15 or mean > 120:
+                    c = img.copy()
+
+                    name = "img{}{}_at_page{}.png".format(img_num, n, i)
+                    is_success, im_buf_arr = cv2.imencode(".png", c[y1:y2, x1:x2])
+                    byte_im = im_buf_arr.tobytes()
+                    byte_im.seek(0)
+                    # check for errors
+                    storage_upload.fileobj2url(byte_im, name)
+
+                    # cv2.imwrite("img{}{}_at_page{}.png".format(img_num, n, i), c[y1:y2, x1:x2])
+                    img_num += 1
+
+        os.remove(os.path.join(workpath, 'tmp.png'))
+        os.remove(tem_pdf_path)
