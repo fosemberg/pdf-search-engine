f17fe6e3d44bfb36518e283fdecc6809eb4bd538
fosemberg <fosemberg@gmail.com>
2020-05-08 18:19:49 +0300

    add-search-results-warning-alerts add warning alert

--- front/src/components/SearchResult/SearchResult.tsx
+++ front/src/components/SearchResult/SearchResult.tsx
@@ -20,44 +20,57 @@ const SearchResult: React.FC<SearchResultProps> = (
     searchResponse = {},
     className= '',
   }
-) => (
+) => {
+  return (
   <div className={`SearchResult ${className}`}>
     {
       searchResponse !== false
         ? Object.keys(searchResponse).length !== 0
-          ? Object.keys(searchResponse).map(
-            componentName => (
-              Object.entries(searchResponse[componentName])
-                .sort(([page1, url1], [page2, url2]) => +page1 - +page2)
-                .map(([page, content]) => (
-                  <div key={page} className="container">
-                    <SearchResultPageModal url={content["url"]} show={false} title={`Page ${page}`}/>
-                    <div className='SearchResult__page'>
-                      {Object.keys(content['images']).length === 0 ? (
-                        <Document
-                          file={content['url']}
-                          loading={<Loader/>}
-                        >
-                          <Page
-                            pageNumber={1}
-                            className="SearchResultPageModal__page"
-                          />
-                        </Document>) : (
-                          <>
-                            {Object.entries(content['images']).map(
-                              ([image_num, image_url]) =>
-                                <Image src={image_url} fluid/>
-                            )}
-                          </>)}
+          ? Object.keys(searchResponse[Object.keys(searchResponse)[0]]).length !== 0
+            ? Object.keys(searchResponse).map(
+              componentName => (
+                Object.entries(searchResponse[componentName])
+                  .sort(([page1, url1], [page2, url2]) => +page1 - +page2)
+                  .map(([page, content]) => (
+                    <div key={page} className="container">
+                      <SearchResultPageModal
+                        url={content["url"]}
+                        show={false}
+                        title={`Page ${page}`}
+                      />
+                      <div className='SearchResult__page'>
+                        {Object.keys(content['images']).length === 0 ? (
+                          <Document
+                            file={content['url']}
+                            loading={<Loader/>}
+                          >
+                            <Page
+                              pageNumber={1}
+                              className="SearchResultPageModal__page"
+                            />
+                          </Document>) : (
+                            <>
+                              {Object.entries(content['images']).map(
+                                ([imageNum, imageUrl]) =>
+                                  <Image
+                                    key={imageNum}
+                                    src={imageUrl}
+                                    fluid
+                                  />
+                              )}
+                            </>
+                        )}
+                      </div>
                     </div>
-                  </div>
-                ))
+                  ))
+              )
             )
-          )
-          : <Alert variant='warning'>Nothing found for this request</Alert>
+            : <Alert variant='warning'>Nothing found in component with this name</Alert>
+          : <Alert variant='warning'>There are no components with this name</Alert>
         : <Alert variant='danger'>We have an error on the server, we are already fixing</Alert>
     }
   </div>
 )
+}
 
 export default SearchResult;
